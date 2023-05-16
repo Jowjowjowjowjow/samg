41036d397ca2c0a66c1c0cbaedf9bfe5d6a19c15
jowjowjowjowjow <jonathanlbs@gmail.com>
2023-05-16 19:52:14 -0300

    tirando porta da url base do front-end

--- src/api/instance.js
+++ src/api/instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     //baseURL: 'http://localhost:3000/',
-    baseURL: 'https://samg.projetosccetunirio.com.br:80/',
+    baseURL: 'https://samg.projetosccetunirio.com.br/',
 });
 
 export default instance
