35c6132aa57266c0bdd122b03953466ca4f5f56b
jowjowjowjowjow <jonathanlbs@gmail.com>
2023-05-16 19:29:08 -0300

    mudançças para mirar no backend externo

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ fastify.post('/upload', async (req, reply) => {
 })
 
 // Run the server!
-fastify.listen({ port: 3000 }, function (err, address) {
+fastify.listen({ port: 80, host: '0.0.0.0' }, function (err, address) {
     if (err) {
         fastify.log.error(err)
         process.exit(1)
