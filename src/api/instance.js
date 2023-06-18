c2a8e690975d802fab4f78e00c1042c2576fd863
phrj94 <ph.rj.94@hotmail.com>
2023-06-18 11:52:44 -0300

    Ajuste de rota pra api

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://samg-api.projetosccetunirio.com.br/',
 });
 
 export default instance
