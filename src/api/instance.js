e62d258023485b1d2a0c6d1ef445858a6535a914
jowjowjowjowjow <jonathanlbs@gmail.com>
2023-06-18 22:07:10 -0300

    mudando url

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://localhost/api',
+    baseURL: 'https://samg-api.projetosccetunirio.com.br/',
 });
 
 export default instance
