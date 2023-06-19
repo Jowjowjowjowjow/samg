6f4c5759126197b742131ed34161c17cec073ffd
jowjowjowjowjow <jonathanlbs@gmail.com>
2023-06-18 21:51:22 -0300

    estou coringando

--- src/api/instance.js
+++ src/api/instance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://samg-api.projetosccetunirio.com.br/',
+    baseURL: 'https://localhost/api',
 });
 
 export default instance
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,14 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://samg-api.projetosccetunirio.com.br/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
   }
 })
