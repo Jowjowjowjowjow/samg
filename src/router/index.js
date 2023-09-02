3394fe9320761e3d601b84fb7c76552b43afb7b9
jowjowjowjowjow <jonathanlbs@gmail.com>
2023-09-02 17:21:44 -0300

    [WIP] Starting to do integralization

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Historico from '../views/Historico.vue'
+import HistoricoIntegralizacao from '../views/HistoricoIntegralizacao.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -7,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Historico
+    },
+    {
+      path: '/integralizacao',
+      name: 'integralizacao',
+      component: HistoricoIntegralizacao
     }
   ]
 })
