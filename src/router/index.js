1022b251cdfe6d3cdfb30e8bb6c5a5f48f524258
phrj94 <ph.rj.94@hotmail.com>
2023-10-14 16:42:07 -0300

    Incluindo página inicial

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Historico from '../views/Historico.vue'
 import HistoricoIntegralizacao from '../views/HistoricoIntegralizacao.vue'
+import Inicio from '../views/Inicio.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Historico
+      name: 'inicio',
+      component: Inicio
     },
     {
       path: '/integralizacao',
       name: 'integralizacao',
+      component: Historico
+    },
+    {
+      path: '/progresso',
+      name: 'progresso',
       component: HistoricoIntegralizacao
-    }
+    },
+    
   ]
 })
 
